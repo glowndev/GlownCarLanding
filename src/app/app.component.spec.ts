b329aa7328d3b5de600bf35c8c78491c57a0450f
AlbertoDgz <diazgaravitos@gmail.com>
2023-11-11 21:58:47 -0500

    cambiar karma y jasmine por jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HelloWorldModule } from './hello-world/hello-world.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HelloWorldModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
     }).compileComponents();
   });
@@ -29,7 +31,10 @@ describe('AppComponent', () => {
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('glownCardLanding app is running!');
+    
+    const appHelloWorld = fixture.nativeElement.querySelector('app-hello-world');
+    const helloWorldContent = appHelloWorld.querySelector('h1').textContent;
+
+    expect(helloWorldContent).toContain('Hola Mundo desde Angular');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { HelloWorldModule } from './hello-world/hello-world.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HelloWorldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
